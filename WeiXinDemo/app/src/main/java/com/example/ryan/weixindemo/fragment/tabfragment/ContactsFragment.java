6c9a42d46d87c6e0587178168ade53f3e25dcaeb
ryanchen <ryan.chen@harman.com>
2016-01-06 00:21:54 +0800

    add file

--- WeiXinDemo/app/src/main/java/com/example/ryan/weixindemo/fragment/tabfragment/ContactsFragment.java
+++ WeiXinDemo/app/src/main/java/com/example/ryan/weixindemo/fragment/tabfragment/ContactsFragment.java
@@ -1,22 +1,23 @@
 package com.example.ryan.weixindemo.fragment.tabfragment;
 
-import android.app.LoaderManager;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.CursorLoader;
-import android.content.Loader;
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract.CommonDataKinds;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.ryan.weixindemo.R;
-import com.example.ryan.weixindemo.common.AppConfig;
 import com.example.ryan.weixindemo.infoobject.Contact;
 import com.example.ryan.weixindemo.view.SectionIndexAdapter;
 
@@ -26,12 +27,38 @@ import java.util.List;
 /**
  * Created by ryan on 12/30/15.
  */
-public class ContactsFragment extends BaseFragmen {
+public class ContactsFragment extends BaseFragmen implements LoaderManager.LoaderCallbacks<Cursor> {
     public static final int CONTACT_QUERY_LOADER = 0;
+    public static final String TAG = "ContactsFragment";
     public static final String QUERY_KEY = "query";
     private ListView listView;
     private SectionIndexAdapter mSectionIndexAdapter;
     private List<Contact> contacts = new ArrayList<>();
+
+    // The column index for the _ID column
+    private static final int CONTACT_ID_INDEX = 0;
+    // The column index for the LOOKUP_KEY column
+    private static final int LOOKUP_KEY_INDEX = 1;
+
+    @SuppressLint("InlinedApi")
+    private static final String SELECTION =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?" :
+                    ContactsContract.Contacts.DISPLAY_NAME + " LIKE ?";
+    // Defines a variable for the search string
+    private String mSearchString;
+    // Defines the array to hold values that replace the ?
+    private String[] mSelectionArgs = {mSearchString};
+
+    @SuppressLint("InlinedApi")
+    private static final String[] PROJECTION = {
+            Contacts._ID,
+            Contacts.LOOKUP_KEY,
+            Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.HONEYCOMB ?
+                    Contacts.DISPLAY_NAME_PRIMARY :
+                    Contacts.DISPLAY_NAME
+    };
 
     @Nullable
     @Override
@@ -45,107 +72,109 @@ public class ContactsFragment extends BaseFragmen {
         listView = (ListView) view.findViewById(R.id.contacts_container);
         mSectionIndexAdapter = new SectionIndexAdapter(getActivity());
         listView.setAdapter(mSectionIndexAdapter);
+        initDate();
     }
 
-//    private void initDate() {
-//        for (int i = 0 ; i < AppConfig.CONTACTS.length;i++){
-//            Contact contact = new Contact();
-//            contact.setName(AppConfig.CONTACTS[i]);
-//            contacts.add(contact);
-//        }
-//    }
+    private void initDate() {
+        getLoaderManager().initLoader(ContactsQuery.QUERY_ID, null, this);
+    }
 
-    public class ContactablesLoaderCallbacks implements LoaderManager.LoaderCallbacks<Cursor> {
 
-        Context mContext;
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (id == ContactsQuery.QUERY_ID){
+            return new CursorLoader(getActivity(),
+                    ContactsQuery.CONTENT_URI,
+                    ContactsQuery.PROJECTION,
+                    ContactsQuery.SELECTION,
+                    null,
+                    ContactsQuery.SORT_ORDER);
 
-        public static final String QUERY_KEY = "query";
+        }return null;
+    }
 
-        public static final String TAG = "ContactablesLoaderCallbacks";
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.e(TAG, "getColumnCount=" + data.getCount());
+        if (loader.getId() == ContactsQuery.QUERY_ID) {
+//            String photoUri = data.getString(ContactsQuery.PHOTO_THUMBNAIL_DATA);
 
-        public ContactablesLoaderCallbacks(Context context) {
-            mContext = context;
+//            Log.e(TAG, "displayName=" + displayName);
         }
+    }
 
-        @Override
-        public Loader<Cursor> onCreateLoader(int loaderIndex, Bundle args) {
-            // Where the Contactables table excels is matching text queries,
-            // not just data dumps from Contacts db.  One search term is used to query
-            // display name, email address and phone number.  In this case, the query was extracted
-            // from an incoming intent in the handleIntent() method, via the
-            // intent.getStringExtra() method.
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 
 
-            String query = args.getString(QUERY_KEY);
-            Uri uri = Uri.withAppendedPath(
-                   CommonDataKinds.Contactables.CONTENT_FILTER_URI, query);
+    /**
+     * This interface defines constants for the Cursor and CursorLoader, based on constants defined
+     * in the {@link ContactsContract.Contacts} class.
+     */
+    public interface ContactsQuery {
 
+        // An identifier for the loader
+        final static int QUERY_ID = 1;
 
-            // Easy way to limit the query to contacts with phone numbers.
-            String selection =
-                    CommonDataKinds.Contactables.HAS_PHONE_NUMBER + " = " + 1;
+        // A content URI for the Contacts table
+        final static Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
 
-            // Sort results such that rows for the same contact stay together.
-            String sortBy = CommonDataKinds.Contactables.LOOKUP_KEY;
+        // The search/filter query Uri
+        final static Uri FILTER_URI = ContactsContract.Contacts.CONTENT_FILTER_URI;
 
-            return new CursorLoader(
-                    mContext,  // Context
-                    uri,       // URI representing the table/resource to be queried
-                    null,      // projection - the list of columns to return.  Null means "all"
-                    selection, // selection - Which rows to return (condition rows must match)
-                    null,      // selection args - can be provided separately and subbed into selection.
-                    sortBy);   // string specifying sort order
+        // The selection clause for the CursorLoader query. The search criteria defined here
+        // restrict results to contacts that have a display name and are linked to visible groups.
+        // Notice that the search on the string provided by the user is implemented by appending
+        // the search string to CONTENT_FILTER_URI.
+        @SuppressLint("InlinedApi")
+        final static String SELECTION =
+                (Utils.hasHoneycomb() ? ContactsContract.Contacts.DISPLAY_NAME_PRIMARY : ContactsContract.Contacts.DISPLAY_NAME) +
+                        "<>''" + " AND " + ContactsContract.Contacts.IN_VISIBLE_GROUP + "=1";
 
-        }
+        // The desired sort order for the returned Cursor. In Android 3.0 and later, the primary
+        // sort key allows for localization. In earlier versions. use the display name as the sort
+        // key.
+        @SuppressLint("InlinedApi")
+        final static String SORT_ORDER =
+                Utils.hasHoneycomb() ? ContactsContract.Contacts.SORT_KEY_PRIMARY : ContactsContract.Contacts.DISPLAY_NAME;
 
-        @Override
-        public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
-            if (cursor.getCount() == 0) {
-                return;
-            }
-            contacts.clear();
-            // Pulling the relevant value from the cursor requires knowing the column index to pull
-            // it from.
+        // The projection for the CursorLoader query. This is a list of columns that the Contacts
+        // Provider should return in the Cursor.
+        @SuppressLint("InlinedApi")
+        final static String[] PROJECTION = {
 
-            int phoneColumnIndex = cursor.getColumnIndex(CommonDataKinds.Phone.NUMBER);
-            int emailColumnIndex = cursor.getColumnIndex(CommonDataKinds.Email.ADDRESS);
-            int nameColumnIndex = cursor.getColumnIndex(CommonDataKinds.Contactables.DISPLAY_NAME);
-            int lookupColumnIndex = cursor.getColumnIndex(CommonDataKinds.Contactables.LOOKUP_KEY);
-            int typeColumnIndex = cursor.getColumnIndex(CommonDataKinds.Contactables.MIMETYPE);
+                // The contact's row id
+                ContactsContract.Contacts._ID,
 
+                // A pointer to the contact that is guaranteed to be more permanent than _ID. Given
+                // a contact's current _ID value and LOOKUP_KEY, the Contacts Provider can generate
+                // a "permanent" contact URI.
+                ContactsContract.Contacts.LOOKUP_KEY,
 
-            cursor.moveToFirst();
-            // Lookup key is the easiest way to verify a row of data is for the same
-            // contact as the previous row.
-            String lookupKey = "";
-            do {
+                // In platform version 3.0 and later, the Contacts table contains
+                // DISPLAY_NAME_PRIMARY, which either contains the contact's displayable name or
+                // some other useful identifier such as an email address. This column isn't
+                // available in earlier versions of Android, so you must use Contacts.DISPLAY_NAME
+                // instead.
+                Utils.hasHoneycomb() ? ContactsContract.Contacts.DISPLAY_NAME_PRIMARY : ContactsContract.Contacts.DISPLAY_NAME,
 
-                String currentLookupKey = cursor.getString(lookupColumnIndex);
-                if (!lookupKey.equals(currentLookupKey)) {
-                    String displayName = cursor.getString(nameColumnIndex);
-                    Contact contact = new Contact();
-                    contact.setName(displayName);
-                    contacts.add(contact);
-                    lookupKey = currentLookupKey;
-                }
+                // In Android 3.0 and later, the thumbnail image is pointed to by
+                // PHOTO_THUMBNAIL_URI. In earlier versions, there is no direct pointer; instead,
+                // you generate the pointer from the contact's ID value and constants defined in
+                // android.provider.ContactsContract.Contacts.
+                Utils.hasHoneycomb() ? ContactsContract.Contacts.PHOTO_THUMBNAIL_URI : ContactsContract.Contacts._ID,
 
+                // The sort order column for the returned Cursor, used by the AlphabetIndexer
+                SORT_ORDER,
+        };
 
-                // The data type can be determined using the mime type column.
-//                String mimeType = cursor.getString(typeColumnIndex);
-//                if (mimeType.equals(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)) {
-//                    tv.append("\tPhone Number: " + cursor.getString(phoneColumnIndex) + "\n");
-//                } else if (mimeType.equals(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)) {
-//                    tv.append("\tEmail Address: " + cursor.getString(emailColumnIndex) + "\n");
-//                }
-
-
-                // Look at DDMS to see all the columns returned by a query to Contactables.
-                // Behold, the firehose!
-            } while (cursor.moveToNext());
-        }
-
-        @Override
-        public void onLoaderReset(Loader<Cursor> cursorLoader) {
-        }
+        // The query column numbers which map to each value in the projection
+        final static int ID = 0;
+        final static int LOOKUP_KEY = 1;
+        final static int DISPLAY_NAME = 2;
+        final static int PHOTO_THUMBNAIL_DATA = 3;
+        final static int SORT_KEY = 4;
     }
 }
