672536d6e3ecf30e179c556e9bf367013ed4ae0c
ryan.chen <ryan.chen@harman.com>
2016-01-11 18:04:31 +0800

    update 3

--- WeiXinDemo/app/src/main/java/com/example/ryan/weixindemo/fragment/tabfragment/GalleryFragment.java
+++ WeiXinDemo/app/src/main/java/com/example/ryan/weixindemo/fragment/tabfragment/GalleryFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import com.example.ryan.weixindemo.R;
 import com.example.ryan.weixindemo.common.ArgumentKeys;
--- WeiXinDemo/app/src/main/java/com/example/ryan/weixindemo/util/LocalImageLoader.java
+++ WeiXinDemo/app/src/main/java/com/example/ryan/weixindemo/util/LocalImageLoader.java
@@ -149,7 +149,7 @@ public class LocalImageLoader {
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
         //设置缩放比例
-        options.inSampleSize = computeScale(options, viewWidth, viewHeight);
+        options.inSampleSize = calculateInSampleSize(options, viewWidth, viewHeight);
 
         //设置为false,解析Bitmap对象加入到内存中
         options.inJustDecodeBounds = false;
@@ -182,6 +182,58 @@ public class LocalImageLoader {
             inSampleSize = widthScale < heightScale ? widthScale : heightScale;
         }
         return inSampleSize;
+    }
+
+    /**
+     * Calculate an inSampleSize for use in a {@link android.graphics.BitmapFactory.Options} object when decoding
+     * bitmaps using the decode* methods from {@link android.graphics.BitmapFactory}. This implementation calculates
+     * the closest inSampleSize that is a power of 2 and will result in the final decoded bitmap
+     * having a width and height equal to or larger than the requested width and height.
+     *
+     * @param options An options object with out* params already populated (run through a decode*
+     *            method with inJustDecodeBounds==true
+     * @param reqWidth The requested width of the resulting bitmap
+     * @param reqHeight The requested height of the resulting bitmap
+     * @return The value to be used for inSampleSize
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        // BEGIN_INCLUDE (calculate_sample_size)
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+
+            // This offers some additional logic in case the image has a strange
+            // aspect ratio. For example, a panorama may have a much larger
+            // width than height. In these cases the total pixels might still
+            // end up being too large to fit comfortably in memory, so we should
+            // be more aggressive with sample down the image (=larger inSampleSize).
+
+            long totalPixels = width * height / inSampleSize;
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            final long totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels > totalReqPixelsCap) {
+                inSampleSize *= 2;
+                totalPixels /= 2;
+            }
+        }
+        return inSampleSize;
+        // END_INCLUDE (calculate_sample_size)
     }
 
 
